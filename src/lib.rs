cbe7d4e09674674c5b20d060dada4fd2338a6a18
Masterchef365 <duncan.freeman1@gmail.com>
2023-04-29 20:33:48 -0700

    Wtf

--- src/lib.rs
+++ src/lib.rs
@@ -39,14 +39,18 @@ impl ClientState {
         let Some(FrameTime { time, .. }) = io.inbox_first() else { return };
 
         let scene = vec![
-            Line(Vec2::new(1., -10.), Vec2::new(1., 10.)),
-            Line(Vec2::new(-1., -10.), Vec2::new(-1., 10.)),
+            //(Line(Vec2::new(1., -10.), Vec2::new(1., 10.)), WallType::Prism(1.510)),
+            (Line(Vec2::new(1., -10.), Vec2::new(1., 10.)), WallType::Mirror),
+            (Line(Vec2::new(2., -10.), Vec2::new(2., 10.)), WallType::Prism(1./1.510)),
+            (Line(Vec2::new(-1., -10.), Vec2::new(-1., 10.)), WallType::Mirror),
             //Line(Vec2::new(3., 1.), Vec2::new(2., 2.))
         ];
 
+        let lines: Vec<Line> = scene.iter().map(|(line, _)| *line).collect();
+
         io.send(&UploadMesh {
             id: WALLS_RDR,
-            mesh: lines_mesh(&scene, [1.; 3]),
+            mesh: lines_mesh(&lines, [1.; 3]),
         });
 
         let ray = Ray {
@@ -91,12 +95,25 @@ impl UserState for ServerState {
 
    Line(p1.xz(), p2.xz())
    }
+*/
 
 #[derive(Component, Copy, Serialize, Deserialize, Clone, Debug)]
 struct Wall {
-width: f32,
+    width: f32,
+    wall_type: WallType,
 }
-*/
+
+#[derive(Copy, Serialize, Deserialize, Clone, Debug)]
+enum WallType {
+    Mirror,
+    Prism(f32),
+}
+
+impl Default for Wall {
+    fn default() -> Self {
+        Wall { width: 0., wall_type: WallType::Mirror }
+    }
+}
 
 #[derive(Copy, Serialize, Deserialize, Clone, Debug)]
 struct Line(Vec2, Vec2);
@@ -159,18 +176,34 @@ fn reflect(incident: Vec2, normal: Vec2) -> Vec2 {
     incident - 2. * incident.dot(normal) * normal
 }
 
-fn calc_path(mut ray: Ray, scene: &[Line], max_bounces: usize) -> Vec<Vec2> {
+fn refract(incident: Vec2, normal: Vec2, eta: f32) -> Vec2 {
+    // https://registry.khronos.org/OpenGL-Refpages/gl4/html/refract.xhtml
+    let dot = incident.dot(normal);
+    let k = 1.0 - eta.powi(2) * (1.0 - dot.powi(2));
+    if k < 0.0 {
+        Vec2::ZERO
+    } else {
+        eta * incident - (eta * dot + k.sqrt()) * normal
+    }
+}
+
+fn calc_path(mut ray: Ray, scene: &[(Line, WallType)], max_bounces: usize) -> Vec<Vec2> {
     let mut points = vec![ray.origin];
+    let lines: Vec<Line> = scene.iter().map(|(line, _)| *line).collect();
 
     for _ in 0..max_bounces {
-        if let Some((idx, interp)) = intersect_scene(ray, scene) {
-            let line = scene[idx];
+        if let Some((idx, interp)) = intersect_scene(ray, &lines) {
+            let (line, wall_type) = scene[idx];
             let end_pt = line.position(interp);
             points.push(end_pt);
 
             // Mirror reflections
             let normal = line.normal();
-            let new_dir = reflect(ray.dir, normal);
+            let new_dir = 
+            match wall_type {
+                WallType::Mirror => reflect(ray.dir, normal),
+                WallType::Prism(eta) => refract(ray.dir, normal, eta),
+            };
 
             ray = Ray {
                 origin: end_pt,
@@ -195,7 +228,7 @@ fn intersect_scene(ray: Ray, scene: &[Line]) -> Option<(usize, f32)> {
     for (idx, line) in scene.iter().enumerate() {
         if let Some(t) = ray_line_intersect(ray, *line) {
             if let Some((_, prev_t)) = closest {
-                if prev_t < t {
+                if t > prev_t || t < 0. {
                     continue;
                 }
             }
